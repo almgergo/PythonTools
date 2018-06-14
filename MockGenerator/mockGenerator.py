3bc194f65e76d64bf669fdb446d42ec13a93135f
Almasy Gergo <gergo.almasy@loxon.eu>
2018-06-14 15:26:16 +0200

    MockGenerator update?

--- MockGenerator/mockGenerator.py
+++ MockGenerator/mockGenerator.py
@@ -25,8 +25,8 @@ insert_end = ''
 with open('insert_end.txt', 'r') as myfile:
     insert_end=myfile.read()
 	
-root = "P:/liquibaseSave/data/"
-insert_root = "P:/LFM.OTPF/backend/src/main/resources/csv_mock/insert/"
+root = "P:/liquibaseSave/new/data/"
+insert_root = "P:/liquibaseSave/new/insert/"
 directory = os.fsencode(root)
 
 for file in os.listdir(directory):
@@ -34,8 +34,8 @@ for file in os.listdir(directory):
 	print(filename)
 	
 	csvHeader = ''
-	with open(filename, 'rt', encoding='ANSI') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',')
+	with open(filename, 'rt', encoding='UTF-8') as csvfile:
+		reader = csv.reader(csvfile, delimiter=';')
 		csvHeader = next(reader)		
 	
 	shortfilename = filename.split('/')[-1]
@@ -60,12 +60,35 @@ for file in os.listdir(directory):
 			assumed_type = 'NUMERIC'
 		elif col=='VERSION' or  col=='version':
 			assumed_type = 'NUMERIC'
-		elif re.search('default', col, re.IGNORECASE):
+		elif col=='CAPACITY' or col=='capacity':
+			assumed_type = 'NUMERIC'
+		elif col=='DEFAULT_ADDRESS' or  col=='default_address':
 			assumed_type = 'BOOLEAN'
+		elif col=='ACTIVE' or  col=='active':
+			assumed_type = 'BOOLEAN'
+		elif col=='MANDATORy' or  col=='mandatory':
+			assumed_type = 'BOOLEAN'
+		elif re.search('device_', col, re.IGNORECASE):
+			assumed_type = 'STRING'
+		elif re.search('interest_rate', col, re.IGNORECASE):
+			assumed_type = 'NUMERIC'
+		elif re.search('amount', col, re.IGNORECASE):
+			assumed_type = 'NUMERIC'
+		elif re.search('has', col, re.IGNORECASE):
+			assumed_type = 'BOOLEAN'
+		elif re.search('isdefault', col, re.IGNORECASE):
+			assumed_type = 'BOOLEAN'
+		elif re.search('string_id', col, re.IGNORECASE):
+			assumed_type = 'STRING'
 		elif re.search('_id', col, re.IGNORECASE):
 			assumed_type = 'NUMERIC'
+		elif re.search('createdat', col, re.IGNORECASE):
+			assumed_type = 'DATE'			
 		elif re.search('date', col, re.IGNORECASE):
 			assumed_type = 'DATE'
+		elif re.search('_at', col, re.IGNORECASE):
+			assumed_type = 'DATE'
+
 		else:
 			assumed_type = 'STRING'
 
